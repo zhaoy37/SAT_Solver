7d8da91a4cb0b6900317485ee1b415928a92cb68
Yiqi Zhao <yiqizhao@Yiqis-MacBook-Pro.local>
2023-03-07 15:47:19 -0600

    Added yet another positive example for parsing.

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from logic_parser import parse_logic
 # Positive Example:
 print(parse_logic("not (x1 and x2) and x3"))
 
+# Another Positive Example:
+print(parse_logic("x1 or (not (x1 and x2) and x3)"))
+
 # Lexical Error Example:
 # print(parse_logic("not a1 and x2 and x3"))
 
