4379ded7b99da327b26ad0ecbf7776509a1e1a4e
Yiqi Zhao <yiqizhao@Yiqis-MacBook-Pro.local>
2023-03-24 12:54:07 -0500

    Changing descriptions.

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from dpll.dpll_kernel import dpll_kernel
 def main():
     print("Thank you for using EduSATSolver. Please select what you want to do from the following options.")
     print("-----------------------------------------------------------------------------------")
-    print("1. Play with the solver.")
-    print("2. Use the solver to solve some interesting problems.")
+    print("1. Play with the solver we created.")
+    print("2. Use an SMT solver to solve some interesting problems.")
     print("-----------------------------------------------------------------------------------")
     selection = input("Enter your selection here:")
     while not selection.isnumeric() or (not (int(selection) == 1 or int(selection) == 2)):
@@ -20,7 +20,7 @@ def main():
     if int(selection) == 1:
         dpll_kernel()
     else:
-        print("For instructions on using the solver for some interesting problems, please see the README file")
+        print("For instructions on using an SMT solver for some interesting problems, please see the README file")
         print("(and our report if you want to know more details of how some problems are solved.)")
 
 if __name__ == "__main__":
